be130388556b811b3906531732c1bd5479607be5
caizhichao <1294970900@qq.com>
2019-04-07 20:56:38 +0800

    还是用go吧 喜欢类型

---
+++ api/main/index.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/astaxie/beego"
+	"strconv"
+)
+
+type MainController struct {
+	beego.Controller
+}
+type msgPack struct {
+	MainID int
+	SubID  int
+	Args   interface{}
+}
+
+func (this *MainController) Get() {
+	mainID, _ := strconv.Atoi(this.Ctx.Input.Param(":mainID"))
+	subID, _ := strconv.Atoi(this.Ctx.Input.Param(":subID"))
+
+	ret := msgPack{MainID: mainID, SubID: subID}
+	ret.Args= DealMainID(mainID, subID)
+
+	data, err := json.Marshal(ret)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	this.Ctx.WriteString(string(data))
+}
+
+func DealMainID(mainID int, subID int) interface{} {
+
+	switch mainID {
+	case 1:
+		return DealSubID(subID)
+	}
+	return ""
+}
+
+func DealSubID(subID int) interface{} {
+	return "nihao"
+}
+
+func main() {
+	beego.Router("/?:mainID([0-9]+)/?:subID([0-9]+)", &MainController{})
+	beego.Run()
+}
